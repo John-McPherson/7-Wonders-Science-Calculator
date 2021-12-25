39b3f5103b23c2ef9c96fc290326b2f7981f0e44
John-McPherson <john-mac1989@hotmail.co.uk>
2021-12-25 14:19:14 +0000

    add function to work out total point

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 let inputs = document.getElementsByClassName("score-tracker")
 for (let x = 0; x < inputs.length; x++) {
-    inputs[x].addEventListener("change", workout_set_points);
+    inputs[x].addEventListener("change", workout_total_points);
 }
 
 /**
  *Works out score for each indivdual science type based on user input
  */
 function workout_indivdual_score() {
+    console.log(this)
     let score = this.value;
     total_score = score * score;
-    console.log(this.id + total_score);
+    return total_score
 }
 /**
  * Works out score for sets of science
@@ -20,10 +21,18 @@ function workout_set_points() {
     for (let x = 0; x < basic.length; x++) {
         if (basic[x].value < sets) {
             sets = basic[x].value
-            console.log("updating sets..." + sets)
         }
-
     }
     points = sets * 7;
     return points
+}
+/**
+ * works out total points
+ */
+function workout_total_points() {
+    let total_sets = workout_set_points()
+    console.log("set points " + total_sets)
+    let total_indivdual = workout_indivdual_score()
+    console.log("indivdual points " + total_indivdual)
+
 }
