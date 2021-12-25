3e1a297c51514edc77f99e61c0562f04dd7fc386
John-McPherson <john-mac1989@hotmail.co.uk>
2021-12-25 14:25:23 +0000

    update workout_indivdual_points to work without this keyword

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,13 @@ for (let x = 0; x < inputs.length; x++) {
  *Works out score for each indivdual science type based on user input
  */
 function workout_indivdual_score() {
-    console.log(this)
-    let score = this.value;
-    total_score = score * score;
-    return total_score
+    let indivdual_score = 0
+    let basic = document.getElementsByClassName("basic")
+    for (let x = 0; x < basic.length; x++) {
+        let score = basic[x].value * basic[x].value
+        indivdual_score += score
+    }
+    return indivdual_score
 }
 /**
  * Works out score for sets of science
